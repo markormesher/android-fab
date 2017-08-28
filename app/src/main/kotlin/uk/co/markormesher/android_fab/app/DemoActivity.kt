d214add7bf79b5e0312792135719fab01d706e8b
Mark Ormesher <me@markormesher.co.uk>
2017-08-28 16:56:40 +0100

    Button with icon, background colour and on-click

--- app/src/main/kotlin/uk/co/markormesher/android_fab/app/DemoActivity.kt
+++ app/src/main/kotlin/uk/co/markormesher/android_fab/app/DemoActivity.kt
@@ -2,50 +2,122 @@ package uk.co.markormesher.android_fab.app
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.demo_activity.*
 import uk.co.markormesher.android_fab.FloatingActionButton
 
 class DemoActivity: AppCompatActivity() {
 
-	private var buttonPosition = FloatingActionButton.POSITION_BOTTOM.or(FloatingActionButton.POSITION_END)
+	private val TOP_START = FloatingActionButton.POSITION_TOP.or(FloatingActionButton.POSITION_START)
+	private val TOP_END = FloatingActionButton.POSITION_TOP.or(FloatingActionButton.POSITION_END)
+	private val BOTTOM_START = FloatingActionButton.POSITION_BOTTOM.or(FloatingActionButton.POSITION_START)
+	private val BOTTOM_END = FloatingActionButton.POSITION_BOTTOM.or(FloatingActionButton.POSITION_END)
+
+	private val BUTTON_BG_COLOURS = intArrayOf(
+			0xff0099ff.toInt(),
+			0xffff9900.toInt(),
+			0xffff0099.toInt(),
+			0xff9900ff.toInt()
+	)
+
+	private val ICONS = intArrayOf(
+			R.drawable.ic_add,
+			R.drawable.ic_cloud,
+			R.drawable.ic_done,
+			R.drawable.ic_swap_horiz,
+			R.drawable.ic_swap_vert
+	)
+
+	private var buttonPosition = BOTTOM_END
+	private var buttonBackgroundColour = 0
+	private var buttonIcon = 0
+
+	private var activeToast: Toast? = null
+	private var clickCounter = 0
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.demo_activity)
 
-		setButtonPositionTopStart.setOnClickListener { btn ->
-			setButtonPosition(FloatingActionButton.POSITION_TOP.or(FloatingActionButton.POSITION_START))
-			btn.isEnabled = false
-		}
-		setButtonPositionTopEnd.setOnClickListener { btn ->
-			setButtonPosition(FloatingActionButton.POSITION_TOP.or(FloatingActionButton.POSITION_END))
-			btn.isEnabled = false
-		}
-		setButtonPositionBottomStart.setOnClickListener { btn ->
-			setButtonPosition(FloatingActionButton.POSITION_BOTTOM.or(FloatingActionButton.POSITION_START))
-			btn.isEnabled = false
-		}
-		setButtonPositionBottomEnd.setOnClickListener { btn ->
-			setButtonPosition(FloatingActionButton.POSITION_BOTTOM.or(FloatingActionButton.POSITION_END))
-			btn.isEnabled = false
+		if (savedInstanceState != null) {
+			buttonPosition = savedInstanceState.getInt("buttonPosition")
+			buttonBackgroundColour = savedInstanceState.getInt("buttonBackgroundColour")
+			buttonIcon = savedInstanceState.getInt("buttonIcon")
 		}
 
+		initControls()
+
 		setButtonPosition(buttonPosition)
-		setButtonPositionBottomEnd.isEnabled = false
+		setButtonBackgroundColour(buttonBackgroundColour)
+		setButtonIcon(buttonIcon)
+
+		fab.setOnClickListener { toast(getString(R.string.toast_click, ++clickCounter)) }
+	}
+
+	override fun onSaveInstanceState(outState: Bundle) {
+		super.onSaveInstanceState(outState)
+		outState.putInt("buttonPosition", buttonPosition)
+		outState.putInt("buttonBackgroundColour", buttonBackgroundColour)
+		outState.putInt("buttonIcon", buttonIcon)
+	}
+
+	private fun initControls() {
+		setButtonPositionTopStart.setOnClickListener { setButtonPosition(TOP_START) }
+		setButtonPositionTopEnd.setOnClickListener { setButtonPosition(TOP_END) }
+		setButtonPositionBottomStart.setOnClickListener { setButtonPosition(BOTTOM_START) }
+		setButtonPositionBottomEnd.setOnClickListener { setButtonPosition(BOTTOM_END) }
+
+		setButtonBackgroundColour1.setOnClickListener { setButtonBackgroundColour(0) }
+		setButtonBackgroundColour2.setOnClickListener { setButtonBackgroundColour(1) }
+		setButtonBackgroundColour3.setOnClickListener { setButtonBackgroundColour(2) }
+		setButtonBackgroundColour4.setOnClickListener { setButtonBackgroundColour(3) }
+
+		setButtonIcon1.setOnClickListener { setButtonIcon(0) }
+		setButtonIcon2.setOnClickListener { setButtonIcon(1) }
+		setButtonIcon3.setOnClickListener { setButtonIcon(2) }
+		setButtonIcon4.setOnClickListener { setButtonIcon(3) }
+		setButtonIcon5.setOnClickListener { setButtonIcon(4) }
+	}
+
+	private fun updateDisabledControls() {
+		setButtonPositionTopStart.isEnabled = buttonPosition != TOP_START
+		setButtonPositionTopEnd.isEnabled = buttonPosition != TOP_END
+		setButtonPositionBottomStart.isEnabled = buttonPosition != BOTTOM_START
+		setButtonPositionBottomEnd.isEnabled = buttonPosition != BOTTOM_END
+
+		setButtonBackgroundColour1.isEnabled = buttonBackgroundColour != 0
+		setButtonBackgroundColour2.isEnabled = buttonBackgroundColour != 1
+		setButtonBackgroundColour3.isEnabled = buttonBackgroundColour != 2
+		setButtonBackgroundColour4.isEnabled = buttonBackgroundColour != 3
+
+		setButtonIcon1.isEnabled = buttonIcon != 0
+		setButtonIcon2.isEnabled = buttonIcon != 1
+		setButtonIcon3.isEnabled = buttonIcon != 2
+		setButtonIcon4.isEnabled = buttonIcon != 3
+		setButtonIcon5.isEnabled = buttonIcon != 4
+	}
+
+	private fun toast(str: String) {
+		activeToast?.cancel()
+		activeToast = Toast.makeText(this, str, Toast.LENGTH_SHORT)
+		activeToast?.show()
 	}
 
 	private fun setButtonPosition(buttonPosition: Int) {
 		this.buttonPosition = buttonPosition
-
 		fab.setButtonPosition(buttonPosition)
-
-		listOf<Button>(
-				setButtonPositionTopEnd,
-				setButtonPositionTopStart,
-				setButtonPositionBottomEnd,
-				setButtonPositionBottomStart
-		).forEach({ it.isEnabled = true })
+		updateDisabledControls()
 	}
 
+	private fun setButtonBackgroundColour(buttonBackgroundColour: Int) {
+		this.buttonBackgroundColour = buttonBackgroundColour
+		fab.setButtonBackgroundColour(BUTTON_BG_COLOURS[buttonBackgroundColour])
+		updateDisabledControls()
+	}
+
+	private fun setButtonIcon(buttonIcon: Int) {
+		this.buttonIcon = buttonIcon
+		fab.setButtonIconResource(ICONS[buttonIcon])
+		updateDisabledControls()
+	}
 }
--- fab/src/main/kotlin/uk/co/markormesher/android_fab/FloatingActionButton.kt
+++ fab/src/main/kotlin/uk/co/markormesher/android_fab/FloatingActionButton.kt
@@ -1,21 +1,24 @@
 package uk.co.markormesher.android_fab
 
 import android.content.Context
+import android.graphics.drawable.GradientDrawable
 import android.os.Build
+import android.support.annotation.DrawableRes
+import android.support.v7.widget.CardView
 import android.util.AttributeSet
-import android.view.View
-import android.view.ViewGroup
 import android.widget.RelativeLayout
+import kotlinx.android.synthetic.main.floating_action_button.view.*
+import kotlinx.android.synthetic.main.floating_action_button_inner.view.*
 import uk.co.markormesher.android_fab.extensions.clearParentAlignmentRules
 import uk.co.markormesher.android_fab.fab.R
 
 class FloatingActionButton: RelativeLayout {
 
-	val containerView by lazy { findViewById<ViewGroup>(R.id.container) }
-	val coverView by lazy { findViewById<View>(R.id.cover) }
-	val buttonView by lazy { findViewById<View>(R.id.buttonCard) }
-
 	private var buttonPosition = POSITION_BOTTOM.or(POSITION_END)
+	private var buttonBackgroundColour = 0xff0099ff.toInt()
+	private var buttonIconResource = 0
+
+	private var onClickListener: OnClickListener? = null
 
 	companion object {
 		const val POSITION_TOP = 1
@@ -41,26 +44,38 @@ class FloatingActionButton: RelativeLayout {
 		initView(attrs)
 	}
 
-	private fun initView(rawAttrs: AttributeSet?) {
+	private fun initView(attrs: AttributeSet?) {
 		isSaveEnabled = true
 		inflate(context, R.layout.floating_action_button, this)
+		applyAttributes(attrs)
+		applyListeners()
 
+		cover_view.alpha = 0f
+	}
+
+	private fun applyAttributes(rawAttrs: AttributeSet?) {
 		val attrs = context.theme.obtainStyledAttributes(rawAttrs, R.styleable.FloatingActionButton, 0, 0)
 		try {
-			setButtonPosition(attrs.getInteger(R.styleable.FloatingActionButton_position, buttonPosition))
-
+			setButtonPosition(attrs.getInteger(R.styleable.FloatingActionButton_buttonPosition, buttonPosition))
+			setButtonBackgroundColour(attrs.getColor(R.styleable.FloatingActionButton_buttonBackgroundColour, buttonBackgroundColour))
+			setButtonIconResource(attrs.getResourceId(R.styleable.FloatingActionButton_buttonIcon, 0))
 		} finally {
 			attrs.recycle()
 		}
+	}
 
-		coverView.alpha = 0f
+	private fun applyListeners() {
+		button_card.setOnClickListener {
+			// TODO: check for speed-dial mode
+			onClickListener?.onClick(this)
+		}
 	}
 
 	fun setButtonPosition(position: Int) {
 		this.buttonPosition = position
 
-		val buttonLayoutParams = buttonView.layoutParams as RelativeLayout.LayoutParams
-		val coverLayoutParams = coverView.layoutParams as RelativeLayout.LayoutParams
+		val buttonLayoutParams = button_card.layoutParams as RelativeLayout.LayoutParams
+		val coverLayoutParams = cover_view.layoutParams as RelativeLayout.LayoutParams
 		buttonLayoutParams.clearParentAlignmentRules()
 		coverLayoutParams.clearParentAlignmentRules()
 
@@ -99,8 +114,29 @@ class FloatingActionButton: RelativeLayout {
 			coverLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
 		}
 
-		buttonView.layoutParams = buttonLayoutParams
-		coverView.layoutParams = coverLayoutParams
+		button_card.layoutParams = buttonLayoutParams
+		cover_view.layoutParams = coverLayoutParams
 	}
 
+	fun setButtonBackgroundColour(colour: Int) {
+		this.buttonBackgroundColour = colour
+		if (Build.VERSION.SDK_INT >= 21) {
+			(button_card as CardView).setCardBackgroundColor(colour)
+		} else {
+			(button_card.background as GradientDrawable).setColor(colour)
+		}
+	}
+
+	fun setButtonIconResource(@DrawableRes icon: Int) {
+		this.buttonIconResource = icon
+		if (icon <= 0) {
+			icon_container.setBackgroundResource(0)
+		} else {
+			icon_container.setBackgroundResource(icon)
+		}
+	}
+
+	override fun setOnClickListener(listener: OnClickListener?) {
+		onClickListener = listener
+	}
 }
